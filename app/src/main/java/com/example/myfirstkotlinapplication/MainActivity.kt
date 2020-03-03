2a53950b84494e2fe2b98f2fe7b15d0ef39a2cb9
quangha2006 <hdquang206@gmail.com>
2020-03-04 01:40:02 +0700

    No comment

--- app/src/main/java/com/example/myfirstkotlinapplication/MainActivity.kt
+++ app/src/main/java/com/example/myfirstkotlinapplication/MainActivity.kt
@@ -17,12 +17,10 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        var mytextview = findViewById<TextView>(R.id.usage)
+        //var mytextview = findViewById<TextView>(R.id.usage)
         val date = getCurrentDateTime()
         val dateInString = date.toString("yyyy/MM/dd HH:mm:ss")
-        mytextview.text = dateInString
-        listView = findViewById<ListView>(R.id.listView)
-        Setfolder()
+
     }
     fun Date.toString(format: String, locale: Locale = Locale.getDefault()): String {
         val formatter = SimpleDateFormat(format, locale)
@@ -32,20 +30,7 @@ class MainActivity : AppCompatActivity() {
     fun getCurrentDateTime(): Date {
         return Calendar.getInstance().time
     }
-    fun Setfolder() {
 
-        val path = Paths.get("").toAbsolutePath().toString()
-
-        var array : ArrayList<String> = ArrayList()
-
-        File("/sdcard/").walk().forEach {
-            println(it)
-            array.add(it.toString())
-        }
-
-        //var array = arrayOf(path)
-        val adapter = ArrayAdapter<String>( this, android.R.layout.simple_list_item_1, array)
-
-        listView.adapter = adapter
-    }
 }
+//tutorial
+//https://www.learn2crack.com/2017/03/searchview-with-recyclerview.html
