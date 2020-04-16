4a9384201a6ef4ce65db028803ad282127ad25b4
quangha <hdquang206@gmail.com>
2020-04-16 18:38:47 +0700

    Fix compile error

--- app/src/main/java/com/example/myfirstkotlinapplication/MainActivity.kt
+++ app/src/main/java/com/example/myfirstkotlinapplication/MainActivity.kt
@@ -3,7 +3,7 @@ package com.example.myfirstkotlinapplication
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v7.widget.Toolbar
-import kotlinx.android.synthetic.main.activity_main2.*
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -11,8 +11,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        //setContentView(R.layout.activity_main)
-        setContentView(R.layout.activity_main2)
+        setContentView(R.layout.activity_main)
         val toolbar = findViewById<Toolbar>(R.id.toolbar)
         setSupportActionBar(toolbar)
         //===================Fragment=====================================
